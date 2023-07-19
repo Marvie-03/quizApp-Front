86c3ba1225031bac545f9592bb211b101b882f78
Marvie-03 <kayveengee@gmail.com>
2023-07-19 16:55:14 +0100

    Sign-up Initialized
    Initialized SignUP PAGE

--- quiz-app/src/App.js
+++ quiz-app/src/App.js
@@ -1,10 +1,11 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import WelcomeScreen from './WelcomeScreen';
+// import SignupLogin from './Signup';
 
 function App() {
   return (
     <>
-    <WelcomeScreen />
+      <WelcomeScreen />
     
     
     </>
